d47dcaa4d45fdaf3a8b754b663cbca20ef04b553
David H. Bronke <whitelynx@gmail.com>
2019-07-28 11:06:48 -0400

    Use built-in `venv` in newer python instead of requiring `virtualenv`

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 PROJECT_DIR="$(cd "$(dirname "$0")"; pwd)"
 ENV_DIR="$PROJECT_DIR/venv"
 
-[ -d "$ENV_DIR" ] || virtualenv "$ENV_DIR"
+[ -d "$ENV_DIR" ] || python -m venv "$ENV_DIR"
 
 "$ENV_DIR/bin/pip" install -r "$PROJECT_DIR/requirements.txt"
 "$ENV_DIR/bin/pip" install cairocffi[xcb] -U --force-reinstall --no-binary cairocffi
