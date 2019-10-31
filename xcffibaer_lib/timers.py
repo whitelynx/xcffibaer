dadb7d12ff9808456997b0e1aeb1a2301218cc7f
David H. Bronke <whitelynx@gmail.com>
2019-10-31 16:25:41 -0400

    reworked timers to use `asyncio.ensure_future`
    More performance optimization!

--- xcffibaer_lib/timers.py
+++ xcffibaer_lib/timers.py
@@ -1,42 +1,29 @@
 '''Timer functions
 
 '''
+import asyncio
 import time
 
 
-sortedTimers = []
-
-
 def addDelay(delay, callback):
-    currentTime = time.monotonic()
-    addDeadline(currentTime + delay, callback)
+    async def _job():
+        await asyncio.sleep(delay)
+        callback()
+    asyncio.ensure_future(_job())
 
 
 def addInterval(delay, callback):
-    def callbackWrapper():
-        addDelay(delay, callbackWrapper)
-        callback()
-
-    addDelay(delay, callbackWrapper)
+    async def _job():
+        while True:
+            await asyncio.sleep(delay)
+            callback()
+    asyncio.ensure_future(_job())
 
 
 def addDeadline(targetTime, callback):
-    for i, (currTargetTime, _currCallback) in enumerate(sortedTimers):
-        if currTargetTime < targetTime:
-            sortedTimers.insert(i, (targetTime, callback))
-            return
-
-    # We only get here if `targetTime` is less than for any already-registered timer. (or if there are no timers)
-    sortedTimers.append((targetTime, callback))
+    currentTime = time.monotonic()
+    addDelay(max(targetTime - currentTime, 0), callback)
 
 
 def addImmediate(callback):
-    sortedTimers.append((time.monotonic(), callback))
-
-
-def triggerElapsedTimers():
-    currentTime = time.monotonic()
-
-    while sortedTimers and sortedTimers[-1][0] < currentTime:
-        _targetTime, callback = sortedTimers.pop()
-        callback()
+    addDelay(0, callback)
